0aa369f2f0846452991d76dcabc93e57a4d1fb05
Eloá Cardozo Augusto <42654458+eloaca@users.noreply.github.com>
2019-04-11 14:51:53 -0300

    exercicio 3
    exercicio 3

--- src/com/fila/Main.java
+++ src/com/fila/Main.java
@@ -11,28 +11,73 @@ public class Main {
 		Scanner leitura = new Scanner (System.in);
 		
 		// Instanciar a Fila
-		Fila<Integer> f = new Fila();
+		Fila<Double> f = new Fila();
+		Fila<Double> aux = new Fila ();
+		Fila<Double> f2 = new Fila();
 		
+		// contador
 		int i = 0;
 		
-		// entrada
-		int valor = 0;
-		
+		Double valor = 0.0;
+
+		/*
 		// enfileirar
-		while (i != 5) {
+		while (i < 6) {
 			System.out.println("Digite o numero: ");
-			valor = leitura.nextInt();
-			
-			f.insereItem(valor);
+			valor = leitura.nextDouble();
+
+			f.enqueue(valor);
 			i++;
+		}
+		*/
+		f.enqueue(5.6);
+		f.enqueue(7.8);
+		f.enqueue(1.3);
+		f.enqueue(3.2);
+		f.enqueue(4.0);
+		f.enqueue(2.5);
+		
+		while (!f.isEmpty()) {
+			
+			// Logica
+			double primeiroF = f.getItem();
+			f.dequeue();
+			aux.enqueue(primeiroF);
+			double primeiroA = aux.getItem();
+			i = 0;
+			
+			while (!(i == f.getSize())) {
+				primeiroF = f.getItem();
+				f.dequeue();
+				
+				primeiroA = aux.getItem();
+				aux.dequeue();
+				
+				if (primeiroF > primeiroA) {
+					
+					f.enqueue(primeiroF);
+					aux.enqueue(primeiroA);
+				}
+				else {
+					aux.enqueue(primeiroF);
+					f.enqueue(primeiroA);
+				}
+				i++;
+				
+			}
+			primeiroA = aux.getItem();
+			f2.enqueue(primeiroA);
+			
+			while (!aux.isEmpty())
+				aux.dequeue();
 		}
 		
 		System.out.println("Esta é sua fila\n");
 		
 		// desemfileirar
-		while (!f.isVazia()) {
-			System.out.println("" +f.getItem());
-			f.removeItem();
+		while (!f2.isEmpty()) {
+			System.out.println("" +f2.getItem());
+			f.dequeue();
 		}
 		
 	}
