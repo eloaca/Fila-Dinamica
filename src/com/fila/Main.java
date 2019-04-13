6e8f07924b54b5f44395e734a789183817f27ee3
eloaca <42654458+eloaca@users.noreply.github.com>
2019-04-13 20:33:02 -0300

    Exercicio 2
    Exercício 2. Criar um programa que permita inserir um elemento depois do enésimo elemento
    de uma fila.

--- src/com/fila/Main.java
+++ src/com/fila/Main.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
+	/**
+	 * Aqui é a main
+	 * @param args
+	 * @throws Exception
+	 */
+	
+	//Exercício 2. Criar um programa que permita inserir um elemento depois do 
+	//enésimo elemento de uma fila
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
@@ -12,29 +21,95 @@ public class Main {
 		
 		// Instanciar a Fila
 		Fila<Integer> f = new Fila();
+		Fila<Integer> aux = new Fila();
 		
 		int i = 0;
 		
 		// entrada
 		int valor = 0;
 		
+		System.out.println ("Ola usuário. Qual é o tamanho da sua fila ? ");
+		int tamanho = leitura.nextInt();
+		
 		// enfileirar
-		while (i != 5) {
+		while (i < tamanho) {
 			System.out.println("Digite o numero: ");
 			valor = leitura.nextInt();
 			
-			f.insereItem(valor);
+			f.enqueue(valor);
 			i++;
 		}
 		
-		System.out.println("Esta é sua fila\n");
+		// reiniciar contador 
+		i = 0;
 		
-		// desemfileirar
-		while (!f.isVazia()) {
-			System.out.println("" +f.getItem());
-			f.removeItem();
+		// criar um novo contador
+		int c = 1;
+		
+		// numero a adicionar
+		System.out.println ("Qual numero voce deseja adicionar ? ");
+		valor = leitura.nextInt();
+		
+		// posicao a considerar
+		System.out.println ("Qual posicao da fila voce deseja colocar o numero ");
+		int posicao = leitura.nextInt();
+		
+		// Pegar o tamanho da filha
+		tamanho = f.getSize();
+
+		// Ele pode adicionar no final da fila mas nao mais que isso
+		if (posicao > tamanho+1 || posicao < 0) {
+			System.err.println("Nao é possivel inserir nessa posicao. ");
+			System.exit(0);
 		}
 		
-	}
+		//Se ele quiser adicionar no fim da fila, ja adiciona direto
+		if (posicao == tamanho+1) {
+			f.enqueue(valor);
+		}
+		else if (posicao == 1) {
+			while (!f.isEmpty()) {
+				int primeiro = f.getItem();
+				aux.enqueue(primeiro);
+				f.dequeue();
+			} //final do while fila vazia
+			f.enqueue(valor);
+			i++; // 
+		}
+		else { // se ele quiser adicionar em outra posicao, precisa entrar no while  
+			while (!(c == posicao)) {
+				int primeiro = f.getItem();
+				aux.enqueue(primeiro);
+				f.dequeue();
+				c++;
+				tamanho = f.getSize();
+				
+			} // final do while comparacao
+			aux.enqueue(valor);
+		
+			while (!f.isEmpty()) {
+				int primeiro = f.getItem();
+				aux.enqueue(primeiro);
+				f.dequeue();
+			} //final do while fila vazia
+			i++;
+		} // final do elseif
+		
+		// entrar nessa condicao se i for maior que 1
+		if (i > 0) {
+			while (!aux.isEmpty()) {
+				int primeiro = aux.getItem();
+				f.enqueue(primeiro);
+				aux.dequeue();
+			}
+		} // final do while aux vazia
 
+				
+		// desemfileirar
+		System.out.println("Esta é sua fila: \n");
+		while (!f.isEmpty()) {
+			System.out.println("" +f.getItem());
+			f.dequeue();;
+		}	
+	}
 }
